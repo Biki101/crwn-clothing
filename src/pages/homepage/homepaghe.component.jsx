4fdf7fe3fae9884cb3878c448a2cc533a7733e87
Biki101 <thakurbiki52@gmail.com>
2022-03-21 17:32:33 +0545

    broken homepage to smaller components and styleed menu item

---
+++ src/pages/homepage/homepaghe.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../components/menu-item/directory/directory.component";
+
+const Homepage = () => {
+  return (
+    <div className="Homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
